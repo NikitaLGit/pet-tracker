63872b1f1e0cf8710dd79f1bde5cad56be954b7d
NikitaLeonov <nikitaleonovgit@gmail.com>
2025-07-18 12:22:13 +0300

    fix 2

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'devkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ.get('DB_USER')}:\
+        {os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
     app.config['UPLOAD_FOLDER'] = os.path.join(
         app.root_path, 'static', 'uploads')
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
